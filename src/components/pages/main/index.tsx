5b402c4e81920dc3d4b3b2fb2d4dfdabb8ae8ee5
오소현 <sohyeon.oh@hnine.com>
2021-08-02 00:09:07 +0900

    test: main page 테스트 코드를 위한 기본 컴포넌트 구조 작성

--- src/components/pages/main/index.tsx
+++ src/components/pages/main/index.tsx
@@ -1,8 +1,24 @@
+import { Button } from 'antd';
 import React from 'react';
+import mediumLogo from 'images/medium_logo2.png';
+import mediumLogoText from 'images/medium_logo.png';
 
 const Main = () => {
     return(
-        <div>main page</div>
+        <div>
+            <header>
+                <span>
+                    <img src={mediumLogo} alt='medium-logo'/>
+                    <img src={mediumLogoText} alt='medium-logo-title'/>
+                </span>
+                <Button type="text">Write</Button>
+                <Button type="text">Sign in</Button>
+                <Button type="text">Get Started</Button>
+            </header>
+            <main>
+
+            </main>
+        </div>
     );
 };
 
