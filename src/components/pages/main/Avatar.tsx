976c263cf6cd6e983708d3b8d1be02a9e1f1391e
오소현 <sohyeon.oh@hnine.com>
2021-08-03 22:16:12 +0900

    feat: main 컴포넌트 > 헤더부분 완료
    - 현재 하드코딩으로 useInfo 유무 체크
    - useInfo 유무에 따라 메인 헤더 내용 변경

---
+++ src/components/pages/main/Avatar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Avatar from 'antd/lib/avatar/avatar';
+import { UserOutlined } from '@ant-design/icons';
+
+const AvatarComponent = () => {
+    return(
+        <Avatar icon={<UserOutlined />} />
+    )
+};
+
+export default AvatarComponent;
