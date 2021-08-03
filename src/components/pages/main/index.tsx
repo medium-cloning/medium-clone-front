dc36489f370ccbde946baf702c1bb6890629444a
오소현 <sohyeon.oh@hnine.com>
2021-08-03 22:16:12 +0900

    feat: main 컴포넌트 > 헤더부분 완료
    - 현재 하드코딩으로 useInfo 유무 체크
    - useInfo 유무에 따라 메인 헤더 내용 변경

--- src/components/pages/main/index.tsx
+++ src/components/pages/main/index.tsx
@@ -1,33 +1,51 @@
-import { Button } from 'antd';
-import React from 'react';
+import { Button, Col, Row } from 'antd';
+import React, { useEffect, useState } from 'react';
 import mediumLogo from 'images/medium_logo2.png';
 import mediumLogoText from 'images/medium_logo.png';
+import AvatarComponent from './Avatar';
+
 
 const Main = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [openModal, setOpenModal] = useState(false);
 
     const getUserInfo = () => {
         // 유저 정보를 가져와 리턴한다.
     };
 
-    const onOpenModal = () => {
+    const onOpenModal = (e) => {
+        const { value } = e.target;
+
+
         // sign in인지 sign up인지 판단
         // 각 역할에 맞는 title을 모달로 전달
         // 모달 컴포넌트 오픈
-    }
-    return(
-        <div>
-            <header>
-                <span>
-                    <img src={mediumLogo} alt='medium-logo' style={{ width: '100px'}}/>
-                    <img src={mediumLogoText} alt='medium-logo-title' style={{ width: '150px', padding: '25px 25px 25px 0' }}/>
-                </span>
-                <Button type="text" onClick={onOpenModal}>Sign in</Button>
-                <Button type="text" onClick={onOpenModal}>Get Started</Button>
-            </header>
-            <main>
+        setOpenModal(prev => !prev);
+    };
 
-            </main>
-        </div>
+    useEffect(() => {
+        setIsLoggedIn(false);
+    }, [])
+    return(
+        <>
+            <div>
+                <header style={{ display: 'flex', alignItems: 'center', width: '100%', justifyContent: 'space-between' }}>
+                    <span>
+                        <img src={mediumLogo} alt='medium-logo' style={{ width: '100px'}}/>
+                        <img src={mediumLogoText} alt='medium-logo-title' style={{ width: '150px', padding: '25px 25px 25px 0' }}/>
+                    </span>
+                    <div>
+                        <Button type="text" onClick={onOpenModal}>{isLoggedIn ? 'Sign out' : 'Sign in'}</Button>
+                        <Button type="text" onClick={onOpenModal}>{isLoggedIn ? <AvatarComponent /> : 'Get started'}</Button>
+                    </div>
+                    
+                </header>
+                <main>
+
+                </main>
+            </div>
+            {openModal && console.log('모달 오픈')}
+        </>
     );
 };
 
